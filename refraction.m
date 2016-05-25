5e5ebadc02bd17693a7a6c801728ddf52679cfdb
diziourov <daniel.iziourov@protonmail.com>
2016-05-25 21:31:17 +0200

    fix: refraction angles, results presentation

--- refraction.m
+++ refraction.m
@@ -5,18 +5,21 @@ function out_angle = refraction(in_speed, out_speed, in_angle)
 	% in_angle - wave's input angle, unit radian
 	% return:
 	% 	out_angle - wave's output angle, unit radian
+	% There is some sin/cos voodoo because an input angle is in relation to X-axis
 
-  epsilon = 0.00001;
-  angle = abs(in_angle);
+  angle = abs(asin(cos(in_angle)));
 
   sin_crit_angle = in_speed/out_speed;
 
-  out_angle = arcsin(sin(angle)*out_speed/in_speed);
+  out_angle = acos(sin(angle)*out_speed/in_speed);
+
+  % no idea why complex numbers
+  out_angle = real(out_angle);
 
   if in_angle < 0
     out_angle = -out_angle;
   end
 
-  if (crit_angle - epsilon) <= sin(angle) <= (crit_angle + epsilon)
+  if sin(angle) > sin_crit_angle
     out_angle = -out_angle;
   end
