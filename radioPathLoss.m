e304e25c6ead1723f0ef31fe146ef47c37e3441d
kaszczesny <kaszczes@student.agh.edu.pl>
2016-05-25 07:08:45 +0200

    notes for radioPathLoss

--- radioPathLoss.m
+++ radioPathLoss.m
@@ -8,3 +8,5 @@ attenuation ... % [dB/m] until changed to Np/m
 
 %%Underwater Wireless Sensor Network Communication Using Electromagnetic Waves at Resonance Frequency 2.4 GHz
 pathLoss = 10 * log( exp(attenuation * distance) );
+%todo: implement whole equation (2); beta is the same as in radioSpeed
+%todo: verify function correctness by comparing visually with Figure 5. for 2.4 GHz
